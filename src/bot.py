f27579f2b05f51b75bc3d83eb811e0cb2fcbd9d6
Derek Hsieh <derekhsieh@Dereks-MacBook-Pro.local>
2023-05-03 18:16:39 -0500

    Made changes to bot but is in infinite loop

--- src/bot.py
+++ src/bot.py
@@ -18,44 +18,61 @@ class ReversiBot:
     Gets the moves for a single player
     """
 
-    def __init__(self, player) -> None:
+    def __init__(self, player, stub: ReversiStub) -> None:
         self.player = player
-        self.game = ReversiStub(8, 2, False)
-        self.numgame = int(sys.argv[1])
+        self.stub = stub
     
-    def apply_move(self):
-        moves = self.game.available_moves()
+    def get_move(self):
+        moves = self.stub.available_moves
         if len(moves) > 0:
             move = random.choices(moves)
-            self.game.apply_move(move)
-            print (self.game)
-        else:
-            None
+            return move[0]
     
-    def play_game(self):
-        while len(self.game.outcome()) < 2:
-            self.apply_move()
-        return self.game.outcome() 
+def play_game(player1: ReversiBot, player2: ReversiBot, game: ReversiStub):
+    while len(game.outcome) < 2:
+        if game.turn == 1:
+            move = player1.get_move()
+            if move is not None:
+                game.apply_move(move)
+            else:
+                game._turn = 2
+        elif game.turn == 2:
+            move = player2.get_move()
+            if move is not None:
+                game.apply_move(move)
+            else:
+                game._turn = 1
+    return game.outcome 
     
-    def play_num_games(self):
-        player1_wins = 0
-        player2_wins = 0
-        draws = 0
-        for i in range(self.numgame):
-            result = self.play_game()
-            if result[0] == 1:
-                player1_wins += 1
-            if result[0] == 2:
-                player2_wins += 1
-            if result[1] == 1:
-                draws += 1
-        player1_perc = player1_wins / self.numgame
-        player2_perc = player2_wins / self.numgame
-        draw_perc = draws / self.numgame
+def play_num_games(numgames):
+    player1_wins = 0
+    player2_wins = 0
+    draws = 0
+    for i in range(numgames):
+        board = ReversiStub (8,2, False)
+        player1 = ReversiBot(1, board)
+        player2 = ReversiBot(2, board)
+        result = play_game(player1, player2, board)
+        if result[0] == 1:
+            player1_wins += 1
+        if result[0] == 2:
+            player2_wins += 1
+        if result[1] == 1:
+            draws += 1
+    player1_perc = (player1_wins / numgames) * 100
+    player2_perc = (player2_wins / numgames) * 100
+    draw_perc = (draws / numgames) * 100
 
-        print (f"Player 1 wins: {player1_perc}")
-        print (f"Player 2 wins: {player2_perc}")
-        print (f"Ties: {draw_perc}")
+    print (f"Player 1 wins: {player1_perc}")
+    print (f"Player 2 wins: {player2_perc}")
+    print (f"Ties: {draw_perc}")
+
+
+def main():
+    numgame = int(sys.argv[1])
+    play_num_games(numgame)
+
+main()
 
 
 
