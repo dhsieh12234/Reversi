23e97bf9d3739caf8dd12d59f78449c430fa0072
Colby Lundak <colbylundak@Colbys-MacBook-Pro.local>
2023-05-11 15:23:10 -0500

    gui milestone 2

--- src/gui.py
+++ src/gui.py
@@ -2,7 +2,7 @@ import os
 import sys
 import math
 from typing import List, Tuple
-from mocks import ReversiStub, BoardGridType
+from mocks import ReversiMock, BoardGridType
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame  # pylint: disable=wrong-import-position
@@ -20,7 +20,7 @@ class Game_Interface:
     surface : pygame.surface.Surface
     clock : pygame.time.Clock
 
-    def __init__(self, game: ReversiStub = ReversiStub(side, 2, False)):
+    def __init__(self, game: ReversiMock):
         """
         Constructor
 
@@ -29,11 +29,11 @@ class Game_Interface:
             border : int : number of pixels to use as border around elements
             cells_side : int : number of cells on a side of a square bitmap grid
         """
+        self.game = game
         self.window = 600
         self.border = 10
-        self.cells_side = side
-        self.grid = [[None] * self.cells_side for i in range(self.cells_side)]
-        self.tool = "Black"
+        self.cells_side = self.game.size
+        self.grid = self.game.grid
         self.square = (self.window - 2 * self.border) // self.cells_side
         self.players = game.num_players
         # Initialize Pygame
@@ -41,20 +41,15 @@ class Game_Interface:
         # Set window title
         pygame.display.set_caption("BitEdit")
         # Set window size
-        self.surface = pygame.display.set_mode((self.window + self.border + self.cells_side,
+        self.surface = pygame.display.set_mode((self.window + self.border + self.square * 1.5,
                                                 self.window))
         self.clock = pygame.time.Clock()
-
-        self.grid[0][0] = 1
-        self.grid[self.cells_side - 1][self.cells_side - 1] = 1
-        self.grid[self.cells_side - 1][0] = 2
-        self.grid[0][self.cells_side - 1] = 2
 
         self.event_loop()
 
 
 
-    def grid_selector(self, loc) -> None:
+    def piece_placer(self, loc) -> None:
         """
         Understand which point in grid is being selected by click.
 
@@ -70,12 +65,27 @@ class Game_Interface:
             self_x = (x - self.border) // (self.square)
         if self.border < x < grid_edge:
             self_y = (y - self.border) // (self.square)
-        if self.tool == "Black" and self_x is not None and self_y is not None:
-            self.grid[self_y][self_x] = True
-        elif self.tool == "White" and self_x is not None and self_y is not None:
-            self.grid[self_y][self_x] = False
-        elif self.tool == "Fill" and self_x is not None and self_y is not None:
-            self.fill_it_up(self_y, self_x, self.grid[self_y][self_x])
+        if self_x is not None and self_y is not None and self.game.legal_move((self_y, self_x)):
+            self.grid[self_y][self_x] = self.game.turn
+            self.game.apply_move((self_y, self_x))
+        
+    def game_over(self) -> None:
+        player_1_color = (155, 155, 155)
+        player_2_color = (105, 105, 105)
+        if self.game.outcome[0] == 1:
+            win_color = player_1_color
+        if self.game.outcome[0] == 2:
+            win_color = player_2_color
+        self.surface.fill(win_color)
+        my_font = pygame.font.SysFont('Impact', 155)
+        text_surface = my_font.render('GAME OVER', False, (35, 35, 35))
+        self.surface.blit(text_surface, (0, 0))
+        my_font = pygame.font.SysFont('Impact', 117)
+        winner = "PLAYER {} WINS"
+        winner = winner.format(self.game.outcome[0])
+        text_surface = my_font.render(winner, False, (35, 35, 35))
+        self.surface.blit(text_surface, (0, 300))
+
 
 
 
@@ -87,31 +97,53 @@ class Game_Interface:
 
         Returns: nothing
         """
-        cells_side = len(self.grid)
+        player_1_color = (155, 155, 155)
+        player_2_color = (105, 105, 105)
+        turn_color = (0, 0, 0)
+        if self.game.turn == 1:
+            turn_color = player_1_color
+        if self.game.turn == 2:
+            turn_color = player_2_color
+        background_color = (35, 35, 35)
+        board_color = (75, 75, 75)
+        circle_radius = self.square // 2 - 5
 
-        # Background
-        self.surface.fill((17, 0, 141))
+        #Create the Background and Turn
+        turn_center = (self.border + self.square * (self.cells_side + 1), 
+                        self.border + 2.5 * self.square)
+        turn_rect = (self.border + self.square * (self.cells_side + 0.5), 
+                    self.border + 2 * self.square , self.square, self.square)
+        text_loc = (self.border + self.square * (self.cells_side + 0.5), 
+                    self.border + 1.5 * self.square)
+        font_size = int(self.square // 2.2)
 
-        for row in range(cells_side):
-            for col in range(cells_side):
+        self.surface.fill(background_color)
+        my_font = pygame.font.SysFont('Impact', font_size)
+        text_surface = my_font.render('TURN', False, board_color)
+        self.surface.blit(text_surface, text_loc)
+        pygame.draw.rect(self.surface, board_color, turn_rect)
+        pygame.draw.circle(self.surface, turn_color, turn_center, circle_radius)
+
+        
+        #Places Pieces
+        for row in range(self.cells_side):
+            for col in range(self.cells_side):
                 rect = (self.border + col * self.square,
                         self.border + row * self.square,
                         self.square, self.square)
                 circle_center = (self.border + (col + 0.5) * self.square, self.border + (row + 0.5) * self.square)
-                fill = (127, 127, 127)
-                border = True
-                pygame.draw.rect(self.surface, color=fill,
+                pygame.draw.rect(self.surface, board_color,
                                  rect=rect)
-                pygame.draw.rect(self.surface, color=(0, 0, 0),
+                if self.game.legal_move((row, col)):
+                    pygame.draw.rect(self.surface, turn_color, rect=rect)
+                pygame.draw.rect(self.surface, background_color,
                                      rect=rect, width=1)
                 if self.grid[row][col] == 1:
-                    fill = (188, 188, 188)
-                    border = True
-                    pygame.draw.circle(self.surface, (188, 188, 188), circle_center, self.square // 2 - 5)
+                    pygame.draw.circle(self.surface, player_1_color, circle_center, circle_radius)
                 elif self.grid[row][col] == 2:
-                    fill = (66, 66, 66)
-                    border = True
-                    pygame.draw.circle(self.surface, (66, 66, 66), circle_center, self.square // 2 - 5)
+                    pygame.draw.circle(self.surface, player_2_color, circle_center, circle_radius)
+        if self.game.done:
+            self.game_over()
 
     def event_loop(self) -> None:
         """
@@ -128,10 +160,12 @@ class Game_Interface:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if self.game.done:
+                    self.game_over()
                 elif event.type == pygame.MOUSEBUTTONUP:
                     pos2 = pygame.mouse.get_pos()
                     loc2 = [pos2[0], pos2[1]]
-                    self.grid_selector(loc2)
+                    self.piece_placer(loc2)
 
 
                 # Handle any other event types here
@@ -143,4 +177,4 @@ class Game_Interface:
 
 
 if __name__ == "__main__":
-    Game_Interface(ReversiStub(side, 2, False))
+    Game_Interface(ReversiMock(side, 2, True))
