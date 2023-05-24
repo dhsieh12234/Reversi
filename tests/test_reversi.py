48f3ecc10b3a1c649c666ffa04c37de208717bc8
Esme Segall <esmesegall@Esmes-MacBook-Air.local>
2023-05-24 12:48:05 -0500

    all passed

--- tests/test_reversi.py
+++ tests/test_reversi.py
@@ -372,13 +372,11 @@ def test_not_othello3():
         (0, 1)
     ]
     helper_apply_move(game, apply)
-    model_ending = [
-        [2, 2, 2, 2, 2],
-        [2, 2, 3, 2, 1],
-        [2, 2, 2, 1, 1],
-        [2, 2, 1, 1, 1],
-        [2, 1, 1, 1, 1]
-    ]
+    model_ending = [[3, 2, 1, 1, 3],
+                    [3, 2, 2, 3, 2],
+                    [3, 2, 3, 2, 2],
+                    [3, 3, 2, 2, 2],
+                    [3, 2, 2, 1, 1]]
     assert game.grid == model_ending
     assert game.done == True
     assert game.outcome == [2]
