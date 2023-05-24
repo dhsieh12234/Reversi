9574e8d0adcc753a4f2b265f56ddb85f52420666
Dino Zavattini <dzavattini@uchicago.edu>
2023-05-24 00:21:05 -0500

    fixed type annotations on Reversi simulate_moves method

--- src/reversi.py
+++ src/reversi.py
@@ -696,7 +696,7 @@ class Reversi(ReversiBase):
 
     def simulate_moves(self,
                        moves: ListMovesType
-                       ) -> "ReversiBase":
+                       ) -> "Reversi":
         """
         Simulates the effect of making a sequence of moves,
         **without** altering the state of the game (instead,
